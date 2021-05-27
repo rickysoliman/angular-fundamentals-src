630010b5eb28150299aaf5a1f0f10d0b89e1883b
Ricky Marasigan <rickymarasigan@Rickys-MacBook-Pro.local>
2021-05-27 13:15:19 -0700

    completed "Creating a container (smart component)"

--- 01-first-component/app/app.module.ts
+++ 01-first-component/app/app.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
+    // angular modules
     BrowserModule,
-    CommonModule
+    CommonModule,
+    // custom modules
+    PassengerDashboardModule
   ],
   bootstrap: [
     AppComponent
