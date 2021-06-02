9ae3c872ef1d7cf83dc9489f4f381b8cc3d94a10
Ricky Marasigan <rickymarasigan@Rickys-MacBook-Pro.local>
2021-06-02 11:58:57 -0700

    completed the "Wildcard routes for 404 handling" section

--- 01-first-component/app/app.module.ts
+++ 01-first-component/app/app.module.ts
@@ -6,10 +6,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' }
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
